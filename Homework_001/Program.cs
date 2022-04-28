7897b1df08e6932bcdcf1823bfd828dc9750313a
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-04-28 13:07:42 +0300

    add Homework #1 Even numbers quantity at array

---
+++ Homework_001/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+Console.WriteLine("Введите количество элементов массива");
+int qty = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[qty];
+
+FillArray(arr, qty);
+int result = EveNumberQty(arr);
+Console.WriteLine("Количество чётных чисел массива: " + result);
+
+void FillArray( int[] array, int quantity)
+{
+    Console.Write("Массив: { ");
+    for(int i = 0; i < quantity; i++)
+        {
+             array[i] = new Random().Next(100, 1000);
+             Console.Write(array[i]);
+             if(i < quantity - 1) Console.Write(", ");
+        }
+    Console.Write(" }"); 
+    Console.WriteLine();    
+}
+
+int EveNumberQty(int[] array)
+{
+    int evenNumber = 0;
+    for(int i = 0; i < array.Length; i++)
+         {
+             if(array[i] % 2 == 0) evenNumber ++; 
+         }
+    return evenNumber;
+}
